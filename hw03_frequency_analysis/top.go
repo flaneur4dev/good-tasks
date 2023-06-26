2f4729e67a1ee3c8f10e6b322c194be12150982f
Alex Basmanov <alba4ex@gmail.com>
2023-06-26 18:09:26 +0300

    hw03: first implementation

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,51 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"regexp"
+	"sort"
+	"strings"
+)
+
+type word struct {
+	w string
+	c int
+}
+
+var re = regexp.MustCompile(`[\s\t\n\r\.,:!\?]`)
+
+func Top10(s string) []string {
+	m := map[string]int{}
+	for _, w := range re.Split(s, -1) {
+		switch w {
+		case "", "-":
+			continue
+		default:
+			m[strings.ToLower(w)]++
+		}
+	}
+
+	var words []word
+	for k, v := range m {
+		words = append(words, word{k, v})
+	}
+
+	sort.Slice(words, func(i, j int) bool {
+		if words[i].c == words[j].c {
+			return words[i].w < words[j].w
+		} else {
+			return words[i].c > words[j].c
+		}
+	})
+
+	limit := len(words)
+	if limit > 10 {
+		limit = 10
+	}
+
+	res := make([]string, 0, limit)
+	for i := 0; i < limit; i++ {
+		res = append(res, words[i].w)
+	}
+
+	return res
 }
