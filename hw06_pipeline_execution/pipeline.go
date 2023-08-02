e95f57d33d3bdbe8bb95ead3a2ca99bb0cd8065d
Alex Basmanov <alba4ex@gmail.com>
2023-08-02 13:47:53 +0300

    hw06: first implementation

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -9,6 +9,48 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	if in == nil {
+		out := make(Bi)
+		close(out)
+		return out
+	}
+	if len(stages) == 0 {
+		return in
+	}
+
+	ch := in
+
+	if done == nil {
+		for _, s := range stages {
+			ch = s(ch)
+		}
+		return ch
+	}
+
+	for _, s := range stages {
+		out := make(Bi)
+
+		go func(in In) {
+			defer close(out)
+
+			for {
+				select {
+				case <-done:
+					// for range in {
+					// 	// draining 'in' channel to allow existing goroutines to finish
+					// }
+					return
+				case v, ok := <-in:
+					if !ok {
+						return
+					}
+					out <- v
+				}
+			}
+		}(ch)
+
+		ch = s(out)
+	}
+
+	return ch
 }
